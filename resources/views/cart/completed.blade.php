1bab5a70e843b6ef6f25fbaf227e981d03a72bfe
Jorge Murta <jmurta.jm@gmail.com>
2015-08-20 16:28:46 +0100

    Change Styling

--- resources/views/cart/completed.blade.php
+++ resources/views/cart/completed.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1 style="color:green;">Order Completed!</h1>
+<h1 style="color:green;margin-bottom:30px;">Your Order was Completed!</h1>
 
 @include('cart.partials.cart', [ 'products' => $cart, 'preview' => true])
 
