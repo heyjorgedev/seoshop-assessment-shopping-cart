0e9578401732cfa29b37457db9335ef8b77579e8
Jorge Murta <jmurta.jm@gmail.com>
2015-08-20 01:09:24 +0100

    Add Code Validation

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,13 +3,27 @@
 @section('content')
 
 	@if(!$errors->isEmpty())
-	<div class="alert alert-danger alert-dismissible" role="alert">
 		
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		@if($errors->has('message'))
+			<div class="alert alert-danger alert-dismissible" role="alert">
 		
-		{{ $errors->first('message') }}
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				
+				{{ $errors->first('message') }}
 
-	</div>
+			</div>
+		@endif
+
+		@if($errors->has('code'))
+			<div class="alert alert-danger alert-dismissible" role="alert">
+		
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				
+				{{ $errors->first('code') }}
+
+			</div>
+		@endif
+
 	@endif
 
 	<h1 style="margin-bottom: 30px;">My Cart</h1>
