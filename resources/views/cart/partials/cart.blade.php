3b8089137db91583d5d66d8211c09306f424ade6
Jorge Murta <jmurta.jm@gmail.com>
2015-08-20 15:04:14 +0100

    Preview Mode

--- resources/views/cart/partials/cart.blade.php
+++ resources/views/cart/partials/cart.blade.php
@@ -9,18 +9,23 @@
 		
 		@include('cart.partials.discounts-table', [ 'discounts' => $cart->discounts])
 		
-		<div> <!-- TODO: Coupons -->
+		@if($preview != true)
+		<div>
 			{!! Form::open([ 'url' => action('CartController@postAddDiscount') , 'method' => 'POST']) !!}
 				<input class="form-control" name="code" style="float:left;width:200px;text-transform: uppercase;" placeholder="" type="text">
 				<button class="btn btn-primary" style="float:left; margin-left:-1px;" type="submit">Add Coupon</button>
 			{!! Form::close() !!}
 		</div>
+		@endif
 
 		<div style="clear:both;"></div>
 
 		<h2 style="text-align: left;">Total: {{ $cart->total }}€</h2>
 		
+		@if($preview != true)
 		<div>
 			<a href="{{ action('CartController@getCheckout') }}" class="btn btn-lg btn-success">Checkout</a>
 		</div>
+		@endif
+		
 	@endif
